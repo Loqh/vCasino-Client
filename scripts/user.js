093661c97eb473e507f6b2f28d18739c09ad7769
Guillaume <guillaume.pinheiro@edu.itescia>
2020-12-10 14:28:55 +0100

    signup page working
    home with signup and login redirect + signup page working with name + email + password (no verification)

--- scripts/user.js
+++ scripts/user.js
@@ -1,10 +1,15 @@
 function lasa(event) {
     event.preventDefault();
 console.log(document.getElementById('myid').value);
+console.log(document.getElementById('myid2').value);
+console.log(document.getElementById('myid3').value);
+console.log(document.getElementById('myid4').value);
 console.log('entered lasa()');
 
 axios.post('http://localhost:8080/api/user/create', {
-    name: document.getElementById('myid').value
+    name: document.getElementById('myid').value,
+    email: document.getElementById('myid2').value,
+    password: document.getElementById('myid3').value
   }).then(function (response) {
     console.log('post called client');
   console.log(response);
