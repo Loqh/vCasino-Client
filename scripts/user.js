94a89bdd525492dc5c319e77b25be8d03a0c477e
Guillaume <guillaume.pinheiro@edu.itescia>
2020-12-11 16:32:21 +0100

    Revert "Revert "before vue""
    This reverts commit 9e5d82bd9f862fe272de08a9b59c89593673c8ad.

--- scripts/user.js
+++ scripts/user.js
@@ -1,20 +1,61 @@
-function lasa(event) {
-    event.preventDefault();
-console.log(document.getElementById('myid').value);
-console.log(document.getElementById('myid2').value);
-console.log(document.getElementById('myid3').value);
-console.log(document.getElementById('myid4').value);
-console.log('entered lasa()');
+ function createUser(event) {
+  event.preventDefault();
 
-axios.post('http://localhost:8080/api/user/create', {
-    name: document.getElementById('myid').value,
-    email: document.getElementById('myid2').value,
-    password: document.getElementById('myid3').value
+  if (document.getElementById('name_input').textLength >= 5) {
+    if (document.getElementById('password_input').textLength >= 5) {
+      if (document.getElementById('password_input').value == document.getElementById('password_confirm_input').value) {
+        axios.post('http://localhost:8080/api/user/create', {
+          user_name: document.getElementById('name_input').value,
+          user_email: document.getElementById('email_input').value,
+          user_password: document.getElementById('password_input').value
+        }).then(function (response) {
+          localStorage.setItem("userid", response.data);
+          window.location.href = "games.html";
+        }).catch(function (error) {
+          toastDangerAlert("Server error (username already in use, email in use)", "Cannot create account");
+        });
+      }
+      else {
+        toastDangerAlert("Make sure you entered the same password in both of the password fields", "Cannot create account")
+      }
+    }
+    else {
+      toastDangerAlert("Your password must be at least 5 characters", "Cannot create account")
+    }
+  }
+  else {
+    toastDangerAlert("Your username must be at least 5 characters", "Cannot create account")
+  }
+}
+
+function loginUser(event) {
+  event.preventDefault();
+
+  axios.post('http://localhost:8080/api/user/login', {
+    user_email: document.getElementById('email_input').value,
+    user_password: document.getElementById('password_input').value
   }).then(function (response) {
-    console.log('post called client');
-  console.log(response);
-}).catch(function (error) {
-    console.log(error);
-    console.log('error or server returned error');
+    localStorage.setItem("userid", response.data);
+    window.location.href = "games.html";
+  }).catch(function (error) {
+    toastDangerAlert("Your email and password combination doesn't match.","Cannot login to your account");
+  });
+}
+
+function toastDangerAlert(content, title) {
+  halfmoon.initStickyAlert({
+    content: content,
+    title: title,
+    alertType: "alert-danger",
+    fillType: "filled"
+  });
+}
+
+function toastWarningAlert(content, title) {
+  halfmoon.initStickyAlert({
+    content: content,
+    title: title,
+    alertType: "alert-secondary",
+    fillType: "filled-dm"
   });
 }
